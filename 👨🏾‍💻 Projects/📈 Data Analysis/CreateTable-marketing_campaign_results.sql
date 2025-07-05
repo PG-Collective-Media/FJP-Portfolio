9c8ab8bfd18d74aa4f531716ed7cf5263f1b6e02
fpruitt22 <fredpruitt22@gmail.com>
2025-07-04 21:43:41 -0400

    FPP1-6-added-create-table-script

---
+++ 👨🏾‍💻 Projects/📈 Data Analysis/CreateTable-marketing_campaign_results.sql
@@ -0,0 +1,9 @@
+CREATE TABLE marketing_campaign_results (
+    Customer_ID VARCHAR(50),
+    Group VARCHAR(50),
+    Email_Clicked BOOLEAN,
+    Purchase_Made BOOLEAN,
+    Age INT,
+    Region VARCHAR(100),
+    Income_Bracket VARCHAR(50)
+);
